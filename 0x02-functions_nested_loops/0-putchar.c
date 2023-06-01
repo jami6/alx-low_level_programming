98e5d085fcc46d19bc9216a7979a0631a3c8aa71
jami6 <nantjami@gmail.com>
2023-06-01 01:59:57 -0700

    tasks 0 to 3

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _putchar - print putchar followed by new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *main - prints the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i=0;
+
+	while (i<10)
+	{
+		c='a';
+		
+		while (c<='z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
